9f69996053f75c18985b7151a3f6ba90fdec9c90
Felipe Klussmann <felipeklussmann@hotmail.com>
2017-11-09 18:32:14 -0200

    commit inicial

---
+++ Calculator/Carro.cs
@@ -0,0 +1,67 @@
+﻿namespace Calculator
+{
+    internal class Carro
+    {
+        public int Portas { get; set; }
+        public int Rodas { get; set; }
+        public int Volante { get; set; }
+        public int Tapete { get; set; }
+        public int Banco { get; set; }
+        public int Janela { get; set; }
+        public int Motor { get; set; }
+        public int Vidro { get; set; }
+
+        public int Acelerar(int velocidade)
+        {
+            velocidade++;
+            return velocidade;
+        }
+
+        public int Freiar(int velocidade)
+        {
+            return --velocidade;
+        }
+
+
+        public Carro ConfigureAstra(Carro astra)
+        {
+            astra.Banco = 4;
+            astra.Motor = 1;
+            astra.Rodas = 4;
+
+            return astra;
+        }
+
+        public Carro ConfigureAstra()
+        {
+            Carro vectra = new Carro();
+
+            vectra.Banco = 12;
+            vectra.Motor = 12;
+            vectra.Rodas = 12;
+
+            return vectra;
+        }
+
+        public Carro ConfigureAstra(int p)
+        {
+            Carro palho = new Carro();
+
+            palho.Banco = 60;
+            palho.Motor = 60;
+            palho.Rodas = 60;
+
+            return palho;
+
+        }
+
+        public Carro ConfigureVectra(Carro vectra)
+        {
+            vectra.Banco = 8;
+            vectra.Motor = 2;
+            vectra.Rodas = 8;
+
+            return vectra;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operacao = string.Empty;
+
+            while (!operacao.Equals("C"))
+            {
+                Console.WriteLine("Calculadora");
+                Console.WriteLine("Selecione a Operação: ");
+                Console.WriteLine("1. Soma");
+                Console.WriteLine("2. Subtração");
+                Console.WriteLine("3. Multiplição");
+                Console.WriteLine("4. Divisão");
+                Console.WriteLine("C. Fechar");
+
+                operacao = Console.ReadLine();
+                if (operacao == "C")
+                {
+                    break;
+                }
+
+
+                Console.WriteLine("Digite o primeiro número: ");
+                Decimal primeiroNumero = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo número: ");
+                Decimal segundoNumero = Convert.ToDecimal(Console.ReadLine());
+
+                Decimal resultado = 0.00m;
+
+                if (operacao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                    Console.WriteLine(resultado);
+                }
+                else if (operacao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                    Console.WriteLine(resultado);
+                }
+                else if (operacao == "3")
+                {
+                    resultado = primeiroNumero * segundoNumero;
+                    Console.WriteLine(resultado);
+                }
+                else if (operacao == "4")
+                {
+                    resultado = primeiroNumero / segundoNumero;
+                    Console.WriteLine(resultado);
+                }
+                else
+                {
+                    Console.WriteLine("Operação Inválida");
+                }
+            }
+        }
+    }
+}
